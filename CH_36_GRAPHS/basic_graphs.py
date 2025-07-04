99d87753e345f679f69221a41c120140d8d8442d
Sushil Rajeeva Bhandary <sushilrajeeva@gmail.com>
2025-07-04 04:11:07 -0400

    Creating graphs and getting all paths from start to end

--- CH_36_GRAPHS/basic_graphs.py
+++ CH_36_GRAPHS/basic_graphs.py
@@ -97,3 +97,100 @@ adj_set = build_adjacency_set(mixed_edges)
 print("\nString‐indexed graph (sets):")
 print_graph(adj_set)
 
+# Connectivity problem set
+
+# 36.2 GRAPH PATH
+"""
+    Given the adjacnecy list of an undirected graph, graph, and two distinct nodes, node1 and node2, return a simple path from node1 to node2.
+    A simple path does not repeat any nodes. Return an empty array if there is no path from node1 to node2.
+    
+    graph = [
+                [1],                # Node 0
+                [0, 2, 5, 4],       # Node 1
+                [1, 4, 5],          # Node 2
+                [],                 # Node 3
+                [5, 2, 1],          # Node 4
+                [1, 2, 4]           # Node 5
+            ]
+
+    node1 = 0 and node2 = 4 
+    Output = [0, 1, 4] there are other valid outputs also like [0, 1, 2, 5, 4]
+
+    node1 = 0 and node2 = 3
+    output = [] there is no path to node 3
+"""
+
+graph = [
+                [1],                # Node 0
+                [0, 2, 5, 4],       # Node 1
+                [1, 4, 5],          # Node 2
+                [],                 # Node 3
+                [5, 2, 1],          # Node 4
+                [1, 2, 4]           # Node 5
+]
+node1 = 0
+node2 = 4
+
+def getPath(graph: List[List[int]], node1: int, node2: int) -> List[int]:
+    path: List[int] = []
+    v: int = len(graph)
+
+    visited = [False] * v
+
+    def visit(node: int) -> None:
+        if visited[node]:
+            return False
+        visited[node] = True
+        path.append(node)
+
+        if node == node2: return True
+
+        for neighbor in graph[node]:
+            if visit(neighbor): return True
+
+        path.pop()
+
+        return False
+        
+    if visit(node1):
+        return path
+    return []
+
+print("Get Path", getPath(graph, node1, node2))
+
+def getAllPaths(graph: List[List[int]], node1: int, node2: int) -> List[List[int]]:
+    path: List[int] = []
+    result: List[List[int]] = []
+    v: int = len(graph)
+    visited = [False] * v
+
+    def visit(node: int) -> None:
+        if visited[node]:
+            return
+        
+        visited[node] = True
+        path.append(node)
+
+        if node == node2:
+            result.append(path.copy())
+        else:
+            for neighbor in graph[node]:
+                visit(neighbor)
+        path.pop()
+        visited[node] = False
+
+    visit(node1)
+    return result
+    
+
+        
+
+print("Get all paths", getAllPaths(graph, node1, node2))
+
+
+# check if graph is a tree
+
+"""
+    Given non-empty adjacnece list of an undirected graph, graph, return wheather it is a tree. A graph is a tree if it is acyclic and connected
+"""
+
